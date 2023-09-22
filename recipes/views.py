1c82a18e7d62867f9a87b8e8c88268e07c53b127
Pablo Ramon <ramonparaiba@gmail.com>
2023-09-22 12:02:18 -0300

    feat: head and search field style

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Pablo Ramon',
+    })
