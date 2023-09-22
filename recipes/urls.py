72228142d01d66f2f7c9a2fee21269d1a2c28637
Pablo Ramon <ramonparaiba@gmail.com>
2023-09-22 07:08:27 -0300

    feat: template criado

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from recipes.views import home, contact, about
+from recipes.views import home
 
 
 urlpatterns = [
     path('', home),
-    path('contact/', contact),
-    path('about/', about),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
-
-def contact(request):
-    return HttpResponse('Contatin')
-
-def about(request):
-    return HttpResponse('Sobre nói')
+    return render(request, 'recipes/home.html')
